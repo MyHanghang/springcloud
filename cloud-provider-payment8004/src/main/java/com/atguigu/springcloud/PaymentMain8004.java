26f11d9e7b603edee821226edfd1aec149dd11c4
我叫航航航啊 <842160255@qq.com>
2020-03-25 23:55:36 +0800

    zookeeper

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
@@ -0,0 +1,23 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @ClassName PaymentMain8004
+ * @Description TODO
+ * @Author 我叫航航航啊
+ * @Date 2020/3/23 22:49
+ * @Version V2.0
+ **/
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain8004 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8004.class, args);
+    }
+
+}
---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+/**
+ * @ClassName PaymentController
+ * @Description TODO
+ * @Author 我叫航航航啊
+ * @Date 2020/3/23 22:50
+ * @Version V2.0
+ **/
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymentzk() {
+        return "Spring cloud with zookeeper:" + serverPort + "\t" + UUID.randomUUID().toString();
+    }
+}
