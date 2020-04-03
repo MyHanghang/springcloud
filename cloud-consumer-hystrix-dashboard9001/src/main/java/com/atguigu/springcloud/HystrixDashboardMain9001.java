ae61637da760e68664cb28e84eab0e71d9561808
罗智航 <842160255@qq.com>
2020-04-03 17:59:58 +0800

    hystrix服务降级以及熔断、限流3

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * @author Zhihang
+ * @version 1.0
+ * @date 2020/4/3 16:50
+ **/
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.
+                run(HystrixDashboardMain9001.class, args);
+    }
+}
